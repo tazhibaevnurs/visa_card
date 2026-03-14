ee0d091233347870fefc35840f0200a6a756b3ea
neo_nurs <neocoindevelopers@gmail.com>
2026-03-14 15:53:03 +0600

    fff

--- VisaCard/urls.py
+++ VisaCard/urls.py
@@ -1,6 +1,8 @@
 """
 URL configuration for VisaCard project.
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -8,3 +10,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cards.urls')),
 ]
+if settings.STATIC_ROOT:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
